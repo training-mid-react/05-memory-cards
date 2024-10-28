a58ae1496b6463f0c0dc194e7866235e19b054c7
JacoboGarces <jacobo.garces@sofka.com.co>
2024-10-28 14:19:11 -0500

    Create game

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,26 @@
+import { FC, MouseEventHandler, ReactElement, useState } from "react"
+import { FaQuestion } from "react-icons/fa6"
+import './style.scss'
+
+interface Props {
+  icon: ReactElement,
+  onClick: MouseEventHandler<HTMLElement>,
+  isFlipped: boolean
+}
+
+export const Card: FC<Props> = ({ icon, onClick, isFlipped }) => {
+  const innerClass = isFlipped ? 'card__inner card__inner--flipped' : 'card__inner'
+
+  return (
+    <article className="card" onClick={onClick}>
+      <div className={innerClass}>
+        <div className="card__front">
+          <FaQuestion size={50} />
+        </div>
+        <div className="card__back">
+          {icon}
+        </div>
+      </div>
+    </article>
+  )
+}
